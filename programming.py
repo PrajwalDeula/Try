8b78f1a308fbb7bedd50297d0b1f2d14277ce349
PrajwalDeula <69007189+PrajwalDeula@users.noreply.github.com>
2023-10-12 12:21:50 +0545

    Add files via upload

---
+++ programming.py
@@ -0,0 +1,10 @@
+num1 = input("enter num1")
+num2 = input("enter num2")
+
+print("value of num1 before swapping",num1)
+print("value of num2 before swapping", num2)
+
+num1,num2=num2,num1
+
+print("value of num1 after swapping", num1)
+print("value of num2 after swapping", num2)
